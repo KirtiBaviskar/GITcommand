805ed76f83750752b722170c2b24472c38137c33
80017 <arpitap@officebeacon.com>
2018-03-14 11:26:52 +0530

    water

---
+++ hungry.py
@@ -0,0 +1,10 @@
+hungry=input("are you hungry ?")
+if hungry=="yes":
+	print("eat pizza")
+	print("eat burger")
+	print("samosa")
+else:
+thirsty=input("are you thirsty ?")
+if thirsty=="yes":
+	print("drink water")
+
